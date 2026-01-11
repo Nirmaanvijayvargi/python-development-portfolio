8dc56064b7d737a87c681f267ce11d770beed700
Nirmaan Vijay vargi <Nirmaanvijayvargi@gmail.com>
2026-01-11 21:58:18 +0530

    Fresh start: Chapter 1

---
+++ module.py
@@ -0,0 +1,12 @@
+import pyjokes
+
+# print("joke incoming...")
+# This prints a random joke 
+joke=pyjokes.get_joke()
+print(joke)
+
+# so thanks 
+# that was my program
+# yet another line
+# line
+#(using hastag to add notes)
